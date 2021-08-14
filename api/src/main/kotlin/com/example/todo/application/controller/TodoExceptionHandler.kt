4731eb36aceb3083882201a9c208943e714960b8
tecchu11 <agdjmp.ajtt@gmail.com>
2021-08-14 20:54:40 +0900

    Improve handle not found error

--- api/src/main/kotlin/com/example/todo/application/controller/TodoExceptionHandler.kt
+++ api/src/main/kotlin/com/example/todo/application/controller/TodoExceptionHandler.kt
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import java.lang.IllegalArgumentException
 
 private val log = KotlinLogging.logger { }
 
@@ -17,14 +16,7 @@ class TodoExceptionHandler {
     @ExceptionHandler(NotFoundException::class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     fun notFoundHandler(e: NotFoundException): ResponseData<String> {
-        log.error("not found exception is thrown", e)
-        return ResponseData("Failure", "Please check request parameter is correct.")
-    }
-
-    @ExceptionHandler(IllegalArgumentException::class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    fun illegalRequestHandler(e: IllegalArgumentException): ResponseData<String> {
-        log.error("Request is illegal", e)
-        return ResponseData("Failure", "Please check way to request body or parameter.")
+        log.info("not found exception is thrown", e)
+        return ResponseData(HttpStatus.NOT_FOUND.name, "Please check request parameter is correct.")
     }
 }
