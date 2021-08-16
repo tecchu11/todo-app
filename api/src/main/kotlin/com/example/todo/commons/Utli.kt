558e4d381af120a4db98b19b4f19ae6ab2f3fee6
tecchu11 <agdjmp.ajtt@gmail.com>
2021-08-17 00:52:06 +0900

    Implements default error handle controller

---
+++ api/src/main/kotlin/com/example/todo/commons/Utli.kt
@@ -0,0 +1,15 @@
+package com.example.todo.commons
+
+import mu.KotlinLogging
+import org.springframework.http.HttpStatus
+import javax.servlet.RequestDispatcher
+import javax.servlet.http.HttpServletRequest
+
+val log = KotlinLogging.logger { }
+
+fun HttpServletRequest.httpStatus(): HttpStatus {
+    val code = this.getAttribute(RequestDispatcher.ERROR_STATUS_CODE) as? Int?
+    return code?.let {
+        HttpStatus.resolve(it)
+    } ?: HttpStatus.INTERNAL_SERVER_ERROR
+}
