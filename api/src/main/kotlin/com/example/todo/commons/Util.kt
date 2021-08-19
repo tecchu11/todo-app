3214286561df96deaa9f40a5d4e9deb60b8bd547
tecchu11 <agdjmp.ajtt@gmail.com>
2021-08-19 23:47:30 +0900

    Add Extensions function for httpStatus

--- api/src/main/kotlin/com/example/todo/commons/Util.kt
+++ api/src/main/kotlin/com/example/todo/commons/Util.kt
@@ -9,7 +9,5 @@ val log = KotlinLogging.logger { }
 
 fun HttpServletRequest.httpStatus(): HttpStatus {
     val code = this.getAttribute(RequestDispatcher.ERROR_STATUS_CODE) as? Int?
-    return code?.let {
-        HttpStatus.resolve(it)
-    } ?: HttpStatus.INTERNAL_SERVER_ERROR
+    return code?.let { HttpStatus.resolve(it) } ?: HttpStatus.INTERNAL_SERVER_ERROR
 }
---
+++ api/src/test/kotlin/com/example/todo/commons/UtilKtTest.kt
@@ -0,0 +1,37 @@
+package com.example.todo.commons
+
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.http.HttpStatus
+import javax.servlet.RequestDispatcher
+import javax.servlet.http.HttpServletRequest
+
+internal class UtilKtTest {
+
+    @MockK
+    lateinit var httpServletRequest: HttpServletRequest
+
+    @BeforeEach
+    fun setUp() {
+        MockKAnnotations.init(this)
+    }
+
+    @Test
+    @DisplayName("Testing status code when handling error")
+    fun httpStatus() {
+        every { httpServletRequest.getAttribute(RequestDispatcher.ERROR_STATUS_CODE) }
+            .returns(404)
+        assertThat(httpServletRequest.httpStatus())
+            .isEqualTo(HttpStatus.NOT_FOUND)
+
+        every { httpServletRequest.getAttribute(RequestDispatcher.ERROR_STATUS_CODE) }
+            .returns(null)
+        assertThat(httpServletRequest.httpStatus())
+            .isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+}
