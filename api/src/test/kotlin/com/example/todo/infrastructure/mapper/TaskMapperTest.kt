61c3954aa848783766e77db8c6f8bc49cd57d3ce
tecchu11 <agdjmp.ajtt@gmail.com>
2021-08-28 02:32:15 +0900

    Revert "Change test annotation to use SpringBootTest"
    This reverts commit 7437e7c55b74d3a62ef7503420ae1f3b976565c6.

--- api/src/test/kotlin/com/example/todo/infrastructure/mapper/TaskMapperTest.kt
+++ api/src/test/kotlin/com/example/todo/infrastructure/mapper/TaskMapperTest.kt
@@ -5,12 +5,12 @@ import com.example.todo.domain.enumration.TaskStatus
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
-import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ContextConfiguration
 
-@SpringBootTest
+@MybatisTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ContextConfiguration(initializers = [MysqlContainerContextInitializer::class])
 internal class TaskMapperTest {
