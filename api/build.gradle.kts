fc5cf1796cfdf3c69ae07a73124d335835fb1378
tecchu11 <agdjmp.ajtt@gmail.com>
2021-08-28 00:14:46 +0900

    launchScript on bootJar task

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
     id("org.springframework.boot") version "2.5.2"
@@ -58,4 +59,8 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.getByName<BootJar>("bootJar") {
+    launchScript()
 }
