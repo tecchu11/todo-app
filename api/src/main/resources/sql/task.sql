8e3f32572ddc2c99e5655457bc0b35b24c54edae
tecchu11 <agdjmp.ajtt@gmail.com>
2021-09-04 00:00:19 +0900

    Delete non need test data

--- api/src/main/resources/sql/task.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO tasks (task_id, task_summary, task_description, user_id, status_id, registered_at, updated_at) VALUES
-('AAAAAAAAAAAAABBBBBBBBBBBBB', 'test todo task', NULL, 1, 1, default, default);
