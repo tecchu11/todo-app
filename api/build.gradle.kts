9b8bd74e2066a349399f5ffb2e541b0523797abf
tecchu11 <agdjmp.ajtt@gmail.com>
2021-10-10 00:47:07 +0900

    Change the sort order dependencies

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -9,11 +9,11 @@ repositories {
 }
 
 dependencies {
-    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.2.0")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.2.0")
 
-    implementation("net.logstash.logback:logstash-logback-encoder:6.6")
     implementation("com.github.guepardoapps:kulid:2.0.0.0")
+    implementation("net.logstash.logback:logstash-logback-encoder:6.6")
 
     runtimeOnly("mysql:mysql-connector-java")
 
