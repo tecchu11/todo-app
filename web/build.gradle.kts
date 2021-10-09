448e9c3d75488705c962edba3b37b818bb0ffa4c
tecchu11 <agdjmp.ajtt@gmail.com>
2021-10-10 00:14:31 +0900

    Apply lint all project target file

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -1,20 +1,20 @@
 plugins {
-	id("todo.kotlin-conventions")
-	id("todo.kotlin-spring-conventions")
-	id("todo.jib-conventions")
+    id("todo.kotlin-conventions")
+    id("todo.kotlin-spring-conventions")
+    id("todo.jib-conventions")
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
