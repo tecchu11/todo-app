e631cf97e54168d8a5bf10f76b77086137310f01
tecchu11 <agdjmp.ajtt@gmail.com>
2021-10-10 00:20:45 +0900

    Commonality spring-dependencies

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    id("todo.kotlin-conventions")
     id("todo.kotlin-spring-conventions")
     id("todo.jib-conventions")
 }
@@ -10,23 +9,15 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.2.0")
     implementation("org.springframework.boot:spring-boot-starter-security")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
 
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("net.logstash.logback:logstash-logback-encoder:6.6")
     implementation("com.github.guepardoapps:kulid:2.0.0.0")
 
     runtimeOnly("mysql:mysql-connector-java")
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test") {
-        exclude(module = "mockito-core")
-    }
     testImplementation("org.mybatis.spring.boot:mybatis-spring-boot-starter-test:2.2.0")
-    testImplementation("com.ninja-squad:springmockk:3.0.1")
     testImplementation(platform("org.testcontainers:testcontainers-bom:1.16.0"))
     testImplementation("org.testcontainers:mysql")
 }
--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    id("todo.kotlin-conventions")
     id("todo.kotlin-spring-conventions")
     id("todo.jib-conventions")
 }
@@ -11,10 +10,5 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
