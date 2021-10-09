031628aae0340a05946471fe1bcfa7801f5a02f8
tecchu11 <agdjmp.ajtt@gmail.com>
2021-10-10 00:47:32 +0900

    Add comment on buildSrc dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,13 +7,16 @@ repositories {
 }
 
 dependencies {
+    // kotlin-conventions
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
     implementation("org.jlleitschuh.gradle:ktlint-gradle:10.2.0")
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.18.1")
 
+    // spring-kotlin-conventions
     implementation("org.springframework.boot:spring-boot-gradle-plugin:2.5.5")
     implementation("io.spring.gradle:dependency-management-plugin:1.0.11.RELEASE")
     implementation("org.jetbrains.kotlin:kotlin-allopen:1.5.31")
 
+    // google-container-tools/jib conventions
     implementation("gradle.plugin.com.google.cloud.tools:jib-gradle-plugin:3.1.4")
 }
