5f26cad5b0925c83208ff2b28d80ba7b7c983b4d
tecchu11 <agdjmp.ajtt@gmail.com>
2021-11-03 18:33:30 +0900

    Add Display name to test load context

--- web/src/test/kotlin/com/example/todo/TodoApplicationTests.kt
+++ web/src/test/kotlin/com/example/todo/TodoApplicationTests.kt
@@ -1,5 +1,6 @@
 package com.example.todo
 
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -7,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest
 class TodoApplicationTests {
 
     @Test
+    @DisplayName("Test able to load spring context")
     fun contextLoads() {
     }
 }
