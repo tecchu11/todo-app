ed6ae6b3f907a0fe283574fa0bf4c5c49a81727e
tecchu11 <68310167+tecchu11@users.noreply.github.com>
2021-12-03 22:44:30 +0900

    Use kapt instead of annotationProcessor (#76)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,6 +9,7 @@ repositories {
 dependencies {
     // kotlin-conventions
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
     implementation("org.jlleitschuh.gradle:ktlint-gradle:10.2.0")
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.19.0")
 
