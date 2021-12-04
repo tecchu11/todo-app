c1a4dc7f64be9d727470e977ceee2fcc75f6fc62
tecchu11 <agdjmp.ajtt@gmail.com>
2021-12-04 19:31:36 +0900

    Use variables when declared dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,15 +7,23 @@ repositories {
 }
 
 dependencies {
+    val kotlin = "1.6.0"
+    val ktlint = "10.2.0"
+    val detekt = "1.19.0"
+    val springDependency = "1.0.11.RELEASE"
+    val springBoot = "2.6.1"
+    val jib = "3.1.4"
+
+
     // kotlin-conventions
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
-    implementation("org.jlleitschuh.gradle:ktlint-gradle:10.2.0")
-    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.19.0")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin")
+    implementation("org.jlleitschuh.gradle:ktlint-gradle:$ktlint")
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:$detekt")
     // spring-dependency-conventions
-    implementation("io.spring.gradle:dependency-management-plugin:1.0.11.RELEASE")
-    implementation("org.jetbrains.kotlin:kotlin-allopen:1.6.0")
+    implementation("io.spring.gradle:dependency-management-plugin:$springDependency")
+    implementation("org.jetbrains.kotlin:kotlin-allopen:$kotlin")
     // spring-boot-conventions
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:2.6.1")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:$springBoot")
     // google-container-tools/jib conventions
-    implementation("gradle.plugin.com.google.cloud.tools:jib-gradle-plugin:3.1.4")
+    implementation("gradle.plugin.com.google.cloud.tools:jib-gradle-plugin:$jib")
 }
