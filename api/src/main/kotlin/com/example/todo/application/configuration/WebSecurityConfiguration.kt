0c86ccad58742fac9b835e5ec94858f3ec93d364
tecchu11 <agdjmp.ajtt@gmail.com>
2022-08-06 21:37:08 +0900

    Modify spring security config

--- api/src/main/kotlin/com/example/todo/application/configuration/WebSecurityConfiguration.kt
+++ api/src/main/kotlin/com/example/todo/application/configuration/WebSecurityConfiguration.kt
@@ -3,28 +3,30 @@ package com.example.todo.application.configuration
 import com.example.todo.domain.service.TodoAuthenticationUserDetailService
 import com.example.todo.enums.UserRole
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AccountStatusUserDetailsChecker
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider
 
-@Configuration
 @EnableWebSecurity
 class WebSecurityConfiguration(
     private val todoAuthenticationUserDetailService: TodoAuthenticationUserDetailService,
-    private val defaultAccessDeniedHandler: DefaultAccessDeniedHandler
-) : WebSecurityConfigurerAdapter() {
+    private val defaultAccessDeniedHandler: DefaultAccessDeniedHandler,
+    private val authenticationConfiguration: AuthenticationConfiguration
+) {
 
-    override fun configure(http: HttpSecurity?) {
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http
-            ?.httpBasic()?.disable()
-            ?.csrf()?.disable()
-            ?.authorizeRequests()
-            ?.mvcMatchers("/v1/todo/**")?.hasAnyAuthority(UserRole.USER.name, UserRole.ADMIN.name)
+            .httpBasic().disable()
+            .csrf().disable()
+            .authorizeHttpRequests()
+            .mvcMatchers("/v1/todo/**")?.hasAnyAuthority(UserRole.USER.name, UserRole.ADMIN.name)
             ?.mvcMatchers("/v1/admin/**")?.hasAuthority(UserRole.ADMIN.name)
             ?.mvcMatchers("**", "*")?.denyAll()
             ?.anyRequest()?.authenticated()
@@ -33,6 +35,8 @@ class WebSecurityConfiguration(
             ?.and()
             ?.addFilter(preAuthenticatedProcessingFilter())
             ?.sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+        return http.build()
     }
 
     @Bean
@@ -41,6 +45,9 @@ class WebSecurityConfiguration(
             setPreAuthenticatedUserDetailsService(todoAuthenticationUserDetailService)
             setUserDetailsChecker(AccountStatusUserDetailsChecker())
         }
+
+    @Bean
+    fun authenticationManager(): AuthenticationManager? = authenticationConfiguration.authenticationManager
 
     @Bean
     fun preAuthenticatedProcessingFilter(): AbstractPreAuthenticatedProcessingFilter =
