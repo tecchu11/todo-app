3d4cf22f6da39d640b697035f27d65e3da33daed
Tetsu Yamakawa <68310167+tecchu11@users.noreply.github.com>
2022-10-15 23:09:21 +0900

    Feature/use annotation base mapper (#153)
    * Use annotation base mapper
    
    * fmt

--- api/src/main/kotlin/com/example/todo/infrastructure/mapper/TaskMapper.kt
+++ api/src/main/kotlin/com/example/todo/infrastructure/mapper/TaskMapper.kt
@@ -2,14 +2,64 @@ package com.example.todo.infrastructure.mapper
 
 import com.example.todo.domain.entity.Task
 import com.example.todo.domain.entity.TaskEntity
+import org.apache.ibatis.annotations.Insert
 import org.apache.ibatis.annotations.Mapper
+import org.apache.ibatis.annotations.Select
+import org.apache.ibatis.annotations.Update
 
 @Mapper
 interface TaskMapper {
 
+    @Select(
+        """
+            select
+                task_id,
+                task_summary,
+                task_description,
+                user_id,
+                status_id,
+                registered_at,
+                updated_at
+            from
+                tasks
+            where
+                user_id = #{userId}
+        """
+    )
     fun selectAll(userId: Int): List<TaskEntity>
 
+    @Insert(
+        """
+            insert into tasks(
+                task_id,
+                task_summary,
+                task_description,
+                user_id,
+                status_id
+            )
+            values
+            (
+                #{taskId},
+                #{taskSummary},
+                #{taskDescription},
+                #{userId},
+                #{status}
+            )
+        """
+    )
     fun insert(task: Task)
 
+    @Update(
+        """
+        update tasks
+        set
+            task_summary = #{taskSummary},
+            task_description = #{taskDescription},
+            status_id = #{status}
+        where
+            user_id = #{userId}
+            and task_id = #{taskId}
+        """
+    )
     fun update(task: Task): Boolean
 }
