5b702b3b626ad1a8432b52899befe2805d944aa3
tecchu11 <agdjmp.ajtt@gmail.com>
2023-02-08 23:17:24 +0900

    Divide kotlin convention plugin

--- buildSrc/src/main/kotlin/base.kotlin-jvm.gradle.kts
+++ buildSrc/src/main/kotlin/base.kotlin-jvm.gradle.kts
@@ -1,32 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import io.gitlab.arturbosch.detekt.Detekt
 
 plugins {
-    kotlin("jvm")
-    id("org.jlleitschuh.gradle.ktlint")
-    id("io.gitlab.arturbosch.detekt")
     id("base.maven")
+    id("base.kotlin-analysis")
+    kotlin("jvm")
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
-
-detekt {
-    config = files("$rootDir/config/detekt/detekt.yml")
-    buildUponDefaultConfig = true
-    parallel = true
-}
-
-tasks.withType<Detekt>().configureEach {
-    jvmTarget = "17"
-    reports {
-        html.required.set(false)
-        xml.required.set(false)
-        txt.required.set(false)
-        sarif.required.set(false)
-    }
-}
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
