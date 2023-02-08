106ed350382a7e21bfdf8bca5702db719283c709
tecchu11 <agdjmp.ajtt@gmail.com>
2023-02-09 00:08:21 +0900

    Merge sarif report

--- buildSrc/src/main/kotlin/base.kotlin-analysis.gradle.kts
+++ buildSrc/src/main/kotlin/base.kotlin-analysis.gradle.kts
@@ -1,8 +1,13 @@
 import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.report.ReportMergeTask
 
 plugins {
     id("org.jlleitschuh.gradle.ktlint")
     id("io.gitlab.arturbosch.detekt")
+}
+
+val reportMerge by tasks.registering(io.gitlab.arturbosch.detekt.report.ReportMergeTask::class) {
+    output.set(rootProject.layout.buildDirectory.file("reports/detekt/merge.sarif"))
 }
 
 detekt {
@@ -11,12 +16,18 @@ detekt {
     parallel = true
 }
 
-tasks.withType<Detekt>() {
-    jvmTarget = "17"
+tasks.withType<Detekt> {
     reports {
         html.required.set(false)
         xml.required.set(false)
+        md.required.set(false)
         txt.required.set(false)
-        sarif.required.set(false)
+        sarif.required.set(true)
+    }
+
+    finalizedBy(reportMerge)
+
+    reportMerge.configure {
+        input.from(this@withType.sarifReportFile)
     }
 }
