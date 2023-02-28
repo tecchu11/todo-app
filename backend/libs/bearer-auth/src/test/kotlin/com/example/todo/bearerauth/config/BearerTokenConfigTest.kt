87c33c5595bcb3dd1582de5297ba6f101fb9d20d
Tetsu Yamakawa <68310167+tecchu11@users.noreply.github.com>
2023-03-01 01:06:09 +0900

    fix test (#207)

--- backend/libs/bearer-auth/src/test/kotlin/com/example/todo/bearerauth/config/BearerTokenConfigTest.kt
+++ backend/libs/bearer-auth/src/test/kotlin/com/example/todo/bearerauth/config/BearerTokenConfigTest.kt
@@ -3,17 +3,13 @@ package com.example.todo.bearerauth.config
 import org.amshove.kluent.`should be equal to`
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.SpringBootConfiguration
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import java.time.Duration
 import java.time.temporal.ChronoUnit
 
 @SpringBootTest
-@SpringBootConfiguration
 @ActiveProfiles("bearer-token-test")
-@EnableConfigurationProperties(BearerTokenConfig::class)
 class BearerTokenConfigTest {
 
     @Autowired
